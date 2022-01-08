52dadcfd07273b7d7db695e90aee6eec8845f8c5
Simon Liang <no.lhr0909@gmail.com>
2022-01-08 10:05:24 +0800

    fixed the ID issue

--- server/wecom/commands/handlers/new-msg.handler.ts
+++ server/wecom/commands/handlers/new-msg.handler.ts
@@ -3,14 +3,20 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { EventStoreService } from 'server/event-store';
 
 import { NewWecomMessageCommand } from '../new-msg.command';
-import { WecomMessageLog } from '../../models';
+import { WecomMessageLog, WecomIncomingMessageType, WecomIncomingEventMessage } from '../../models';
 
 @CommandHandler(NewWecomMessageCommand)
 export class NewWecomMessageCommandHandler implements ICommandHandler<NewWecomMessageCommand, void> {
   constructor(private readonly eventStore: EventStoreService) {}
 
   public async execute(command: NewWecomMessageCommand): Promise<void> {
-    const id = `${command.open_kfid}-${command.external_userid}`;
+    let id = `${command.open_kfid}-${command.external_userid}`;
+
+    if (command.msgtype === WecomIncomingMessageType.Event) {
+      const event = (command as WecomIncomingEventMessage).event;
+      id = `${event.open_kfid}-${event.external_userid}`;
+    }
+
     const log = await this.eventStore.getAggregate<WecomMessageLog>(WecomMessageLog.name, id);
     log.newMessage(command);
     log.commit();
